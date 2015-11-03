3f91f82f7cb2dfd61b135c62d5814c9ec878bc3c
Eric W. Greene <eric@training4developers.com>
2015-11-03 12:00:38 -0500

    added more demos:

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,88 @@ module.exports = function(grunt) {
 
 	"use strict";
 
+	var
+		path = require("path"),
+		wwwFolder = path.join("app", "www"),
+
+		libsFolder = path.join(wwwFolder, "libs"),
+		jsFolder = path.join(wwwFolder, "js"),
+
+		jsFiles = {},
+		jsMinifyFiles = {},
+		jsCompressFiles = {};
+
+	jsFiles[path.join(jsFolder, "site.js")]	= [
+			path.join(libsFolder, "jquery", "dist", "jquery.js"),
+			path.join(libsFolder, "angular", "angular.js"),
+			path.join(jsFolder, "app.js"),
+			path.join(jsFolder, "app.constants.js"),
+			path.join(jsFolder, "checkin", "controllers", "*.js"),
+			path.join(jsFolder, "reservations", "controllers", "*.js"),
+			path.join(jsFolder, "app_init.js")
+	];
+
+	jsMinifyFiles[path.join(jsFolder, "site.min.js")]	=
+		path.join(jsFolder, "site.js");
+
+	jsCompressFiles[path.join(jsFolder, "site.min.gz.js")]	=
+		path.join(jsFolder, "site.min.js");
+
 	grunt.initConfig({
 		webServer: {
 			port: 8080,
 			rootFolder: "app/www"
+		},
+		uglify: {
+			combine: {
+        options: {
+          compress: false,
+          beautify: {
+            beautify: true,
+            indent_level: 2,
+            comments: true
+          },
+          mangle: false,
+        },
+				files: jsFiles
+			},
+      minify: {
+        options: {
+          compress: {
+            drop_debugger: true,
+            unsafe: true,
+            drop_console: false
+          },
+          beautify: false,
+          mangle: {},
+          screwIE8: true
+        },
+        files: jsMinifyFiles
+      }
+		},
+    compress: {
+      js: {
+        options: {
+          mode: 'gzip'
+        },
+        files: jsCompressFiles
+      }
+    },
+		watch: {
+			js: {
+				files: [
+					path.join(jsFolder, "**", "*.js"),
+					"!" + path.join(jsFolder, "*.min.js")],
+				tasks: ["uglify:combine","compress:js"]
+			}
 		}
 	});
 
-	grunt.registerTask("default", function() {
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-compress");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+
+	grunt.registerTask("web-server", function() {
 
 		const
 			http = require("http"),
@@ -19,7 +93,16 @@ module.exports = function(grunt) {
 			app = express(),
 			webServerConfig = grunt.config("webServer");
 
-		this.async();
+		//this.async();
+
+		app.use("/js", express.static(jsFolder, {
+			setHeaders: function(res, filePath) {
+				res.setHeader("Content-Type", "text/javascript");
+				if (/.gz.js$/.test(filePath)) {
+					res.setHeader("Content-Encoding", "gzip");
+				}
+			}
+		}));
 
 		app.use(express.static(webServerConfig.rootFolder));
 
@@ -29,5 +112,8 @@ module.exports = function(grunt) {
 
 
 	});
+
+	grunt.registerTask("default", "standard dev task",
+		["uglify:combine", "uglify:minify", "compress:js", "web-server", "watch"]);
 
 };
